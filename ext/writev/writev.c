6335f53d0ace35c5c89a4fdd94f27027335170aa
Aaron Patterson <aaron.patterson@gmail.com>
2011-08-19 11:02:35 -0700

    returns number of bytes written

--- ext/writev/writev.c
+++ ext/writev/writev.c
@@ -2,13 +2,26 @@
 #include <ruby/io.h>
 #include <sys/uio.h>
 
+static VALUE
+rb_io_check_io(VALUE io)
+{
+    return rb_check_convert_type(io, T_FILE, "IO", "to_io");
+}
+
+#define rb_sys_fail_path(path) rb_sys_fail(NIL_P(path) ? 0 : RSTRING_PTR(path))
+
 static VALUE rb_writev(VALUE io, VALUE list)
 {
     rb_io_t * fptr;
     struct iovec * iov;
     long i;
+    ssize_t written;
+    VALUE tmp;
 
-    GetOpenFile(io, fptr);
+    tmp = rb_io_check_io(io);
+    GetOpenFile(tmp, fptr);
+    rb_io_check_writable(fptr);
+
     iov = xcalloc(RARRAY_LEN(list), sizeof(struct iovec));
 
     for(i = 0; i < RARRAY_LEN(list); i++) {
@@ -17,9 +30,10 @@ static VALUE rb_writev(VALUE io, VALUE list)
 	iov[i].iov_len = RSTRING_LEN(string);
     }
 
-    writev(fptr->fd, iov, RARRAY_LEN(list));
+    if((written = writev(fptr->fd, iov, RARRAY_LEN(list))) == -1)
+	rb_sys_fail_path(fptr->pathv);
 
-    return Qnil;
+    return LONG2FIX(written);
 }
 
 void Init_writev()
--- test/test_writev.rb
+++ test/test_writev.rb
@@ -10,6 +10,21 @@ class TestWritev < MiniTest::Unit::TestCase
     assert_equal 'helloworld', File.read(file.path)
     file.unlink
   end
+
+  def test_length
+    file = Tempfile.new('foo')
+
+    assert_equal 'helloworld'.bytesize, file.writev(%w{ hello world })
+    file.unlink
+  end
+
+  def test_fail
+    File.open(__FILE__, 'r') do |f|
+      assert_raises(IOError) do
+        f.writev %w{ hello world }
+      end
+    end
+  end
 end
 
 # /* vim: set et sws=2 sw=2: */
